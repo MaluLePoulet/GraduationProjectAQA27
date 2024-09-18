ea6d2bf766e0716d7d00ec044d2904d92cc50467
KhramtsovaV <khramtsova.valeria@gmail.com>
2024-09-18 22:56:44 +0300

    init commit

--- src/test/java/baseEntities/BaseStep.java
+++ src/test/java/baseEntities/BaseStep.java
@@ -1,12 +1,18 @@
 package baseEntities;
 
 import pages.AddProjectPage;
+import pages.LoginPage;
+import pages.ProjectsPage;
 
 public class BaseStep {
     protected AddProjectPage addProjectPage;
+    protected LoginPage loginPage;
+    protected ProjectsPage projectsPage;
 
 
     public BaseStep() {
         addProjectPage = new AddProjectPage();
+        loginPage = new LoginPage();
+        projectsPage = new ProjectsPage();
     }
 }
--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -1,4 +1,30 @@
 package steps;
 
-public class LoginStep {
+import baseEntities.BaseStep;
+import pages.LoginPage;
+import pages.ProjectsPage;
+
+public class LoginStep extends BaseStep {
+
+    public LoginStep() {
+        super();
+    }
+
+    public ProjectsPage successfulLogin(String username, String password) {
+        login(username, password);
+
+        return projectsPage;
+    }
+
+    public LoginPage incorrectLogin(String username, String password) {
+        login(username, password);
+        return loginPage;
+    }
+
+    private void login(String username, String password) {
+        loginPage
+                .setEmail(username)
+                .setPassword(password)
+                .clickLoginButton();
+    }
 }
