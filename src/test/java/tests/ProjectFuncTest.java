6d4d92f73684e8ef31d0fbc1f4101c88a43e2fdc
MaluLePoulet <168747330+MaluLePoulet@users.noreply.github.com>
2024-09-22 02:38:20 +0300

    update to check if Jenkins works fine with Allure

--- src/test/java/tests/ProjectFuncTest.java
+++ src/test/java/tests/ProjectFuncTest.java
@@ -17,7 +17,7 @@ public class ProjectFuncTest extends BaseTest {
     @Test
     public void projectFunctionalityTest() {
         loginStep
-                .successfulLogin(UserDirector.getAdmin())
+                .successfulLogin(UserDirector.getUserWithIncorrectEmail())
                 .createNewProject();
 
         addProjectPage // Тест на проверку превышающих значений
@@ -25,7 +25,7 @@ public class ProjectFuncTest extends BaseTest {
                 .getSummaryInput()
                 .shouldNotBe(empty)
                 .shouldHave(value("This text contains way way way way more than eighty symbols. The exact number is"));
-        $(".maxlength-counter__counter").shouldHave(text("80/80"));
+        $(".maxlength-counter__counter").shouldHave(text("80/81"));
 
         addProjectPage // Тест на проверку всплывающего сообщения
                 .getDefaultAccessTooltip()
