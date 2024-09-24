0e899200d1e4f249bb25b25f5bb877a884aed55c
KhramtsovaV <khramtsova.valeria@gmail.com>
2024-09-24 20:07:11 +0300

    part_of_tests

--- src/test/java/pages/ProjectsPage.java
+++ src/test/java/pages/ProjectsPage.java
@@ -9,7 +9,8 @@ public class ProjectsPage extends BasePage {
     private final SelenideElement PROJECTS_TITLE_LOCATOR = $(".page-title__title");
     private final SelenideElement PROJECT_ADD_BUTTON_LOCATOR = $(".ui.basic.compact.button");
     private final SelenideElement OPEN_PROJECT_LOCATOR = $("a[href='https://aqa27.testmo.net/projects/view/1']");
-
+    private final SelenideElement DELETE_BUTTON_LOCATOR = $("div[data-action='delete']");
+    
     @Override
     protected SelenideElement getPageIdentifier() {
         return PROJECTS_TITLE_LOCATOR;
@@ -21,6 +22,10 @@ public class ProjectsPage extends BasePage {
 
     public SelenideElement getOpenProjectLink() {
         return OPEN_PROJECT_LOCATOR;
+    }
+
+    public SelenideElement getDeleteButton () {
+        return DELETE_BUTTON_LOCATOR;
     }
 
     public AddProjectPage createNewProject() {
